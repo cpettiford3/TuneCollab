6ae54e8f3475134b6dda95ff6aadbaec3e35f069
Courtney Pettiford <courtneypettiford@Courtneys-MacBook-Pro.local>
2018-03-09 23:16:46 -0500

    added welcome & register activities

---
+++ TuneCollab/app/src/main/java/com/example/courtneypettiford/tunecollab/controllers/MainActivity.java
@@ -0,0 +1,8 @@
+package com.example.courtneypettiford.tunecollab.controllers;
+
+/**
+ * Created by courtneypettiford on 3/9/18.
+ */
+
+public class MainActivity {
+}
--- TuneCollab/app/src/main/java/com/example/courtneypettiford/tunecollab/controllers/WelcomeActivity.java
+++ TuneCollab/app/src/main/java/com/example/courtneypettiford/tunecollab/controllers/WelcomeActivity.java
@@ -1,9 +1,11 @@
-package com.example.courtneypettiford.tunecollab;
+package com.example.courtneypettiford.tunecollab.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.courtneypettiford.tunecollab.R;
+
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
